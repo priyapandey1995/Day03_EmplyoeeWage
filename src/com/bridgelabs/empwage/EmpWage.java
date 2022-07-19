ef60ff3b40c07504b9e70a8e5ad4a102f7fe9080
priya <priyapandey8884.blr@gmail.com>
2022-07-20 01:03:40 +0530

    [Priya]:Added Emplyoee Monthly Wage

--- src/com/bridgelabs/empwage/EmpWage.java
+++ src/com/bridgelabs/empwage/EmpWage.java
@@ -6,28 +6,31 @@ public class EmpWage {
 		System.out.println("Welcome to Emplyoee Wage Computation Program");
 		
 		/* UC=3
-		 * to check employee is present or absent or working for part time
-		 * switch statement is used to calculate the employee wage 
+		 * to check employee is present or absent or working for part time.
+		 * switch statement is used to calculate the monthly wage employee wage 
 		 * 
 		 */
 		int isPresent = 1;
 		int wagePerHour = 20;
 		int noOfWorkingHours = 8;
 		int partTimeHours = 4;
+		int workingDaysInAMonth = 20;//monthly
 		int checkAttendance =(int)Math.floor((Math.random()*10)%3);	
 		System.out.println(checkAttendance);
-		switch(checkAttendance){         //UC4 added
+		switch(checkAttendance){         
 		case 0 : 
 			System.out.println("Emplyoee is absent");
 			break;
 		
 		case 1:
 			System.out.println("Emplyoee is present");//uc1 code added
-			System.out.println("Emplyoee Daily wage is" + " " +wagePerHour*noOfWorkingHours);//uc4 code is added
+			System.out.println("Emplyoee Daily Wage is"+" " +wagePerHour * noOfWorkingHours);
+			System.out.println("Emplyoee Monthly wage is" + " " +wagePerHour*noOfWorkingHours*workingDaysInAMonth);//uc5 code is added
 			break;
 		case 2:
 			System.out.println("Part Time");
-			System.out.println("Emplyoee Part Time wage is"+" " + wagePerHour*partTimeHours);
+			System.out.println("Emplyoee part Time wage is" + noOfWorkingHours*wagePerHour);
+			System.out.println("Emplyoee Monthly wage is"+" " + wagePerHour*partTimeHours*workingDaysInAMonth);
 			break;
 		
 	}
