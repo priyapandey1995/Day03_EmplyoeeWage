fc7379ad527ef32ac00789a2faa7f836476652df
priya <priyapandey8884.blr@gmail.com>
2022-07-19 23:37:25 +0530

    [Priya]:Added Emplyoee present Or Absent

--- src/com/bridgelabs/empwage/EmpWage.java
+++ src/com/bridgelabs/empwage/EmpWage.java
@@ -3,8 +3,22 @@ package com.bridgelabs.empwage;
 public class EmpWage {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Emplyoee Wage Computatio Program");
-
+		System.out.println("Welcome to Emplyoee Wage Computation Program");
+		
+		/* UC=1
+		 * to check employee is present or absent
+		 * initially variable isPresent is assigned as 1;and then
+		 * random function is used to get the desired output
+		 */
+		int isPresent = 1;
+		int checkAttendance =(int)Math.floor((Math.random()*10)%2);	
+		System.out.println(checkAttendance);
+		if(checkAttendance == isPresent) {
+			System.out.println("Emplyoee is present");
+		}
+		else {
+			System.out.println("Emplyoee is absent");
 	}
+}
 
 }
