d6a518db0c8b81728a0eac0f3087a172f43e9564
priya <priyapandey8884.blr@gmail.com>
2022-07-19 17:06:35 +0530

    [priya]:added welcome message

---
+++ src/com/bridgelabs/empwage/EmpWage.java
@@ -0,0 +1,10 @@
+package com.bridgelabs.empwage;
+
+public class EmpWage {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Emplyoee Wage Computatio Program");
+
+	}
+
+}
